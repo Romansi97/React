f450423e19139208b27ae88d2c58f7c9334ebee2
Roman Ricciardi <romanezequielr97@gmail.com>
2023-09-05 21:53:05 -0300

    imagen

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import imagenLogo from "../src/Images/calculadora.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="logo-contenedor">
+        <img className="imagen-logo" src={imagenLogo} alt="logo" />
+      </div>
+      <div className="contenedor-principal"></div>
+    </div>
+  );
 }
 
 export default App;
